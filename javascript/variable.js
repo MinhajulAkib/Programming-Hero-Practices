0fbfc828d9a43f32c2fc750944b2a5a77073217d
MinhajulAkib <00akibswe@gmail.com>
2021-07-31 13:21:10 +0600

    variable declarartion type

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,3 +1,13 @@
+// numeric variable
 var price = 10;
 var age = 23;
 var teparature = 37;
+
+// type declaration
+// string type
+var name ="minhaj akib";
+var address = "dhaka bangladesh";
+
+// boolean
+var pass = true;
+var subscribe = false;
