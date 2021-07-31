d787c3e6d83e33266bc9477b0702810ada2cab29
MinhajulAkib <00akibswe@gmail.com>
2021-07-31 16:17:23 +0600

    mathematical operations

--- javascript/mathematical-operation.js
+++ javascript/mathematical-operation.js
@@ -1,26 +1,26 @@
-// var onionPrice = 43;
-// var eggPrice = 42;
-// var eggQuantity =7;
-// var totalPrice = onionPrice + eggPrice;
-// var priceDiffererce = onionPrice - eggPrice;
-// var eggPriceMultiplication = eggPrice * eggQuantity;
-// console.log(totalPrice);
-// console.log(onionPrice);
-// console.log(eggPrice);
-// console.log(priceDiffererce);
-// console.log(eggPriceMultiplication);
+var onionPrice = 43;
+var eggPrice = 42;
+var eggQuantity =7;
+var totalPrice = onionPrice + eggPrice;
+var priceDiffererce = onionPrice - eggPrice;
+var eggPriceMultiplication = eggPrice * eggQuantity;
+console.log(totalPrice);
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(priceDiffererce);
+console.log(eggPriceMultiplication);
 
 
-// var price1 = 31;
-// var price2 = 10;
-// var sum = price1 +price2;
-// var difference = price1 - price2;
-// var multiplication = price1 * price2;
-// var division = price1 / price2;
-// console.log(sum);
-// console.log(difference);
-// console.log(multiplication);
-// console.log(division);
+var price1 = 31;
+var price2 = 10;
+var sum = price1 +price2;
+var difference = price1 - price2;
+var multiplication = price1 * price2;
+var division = price1 / price2;
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(division);
 
 var age = 11;
 age++;
