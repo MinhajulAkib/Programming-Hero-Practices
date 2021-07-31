c1c515ce9d919c5811a75ad495c0d7dc6f5ca397
MinhajulAkib <00akibswe@gmail.com>
2021-07-31 16:50:12 +0600

    conversion

---
+++ javascript/conversion.js
@@ -0,0 +1,17 @@
+var firstName = 'jennie';
+var lastName  = 'kim';
+var fullname = firstName + ' ' + lastName;
+console.log(fullname);
+
+
+// // integer
+// var price = 34;
+
+// // float
+// var price = 44.44;
+
+// convert string to integer
+var onionPrice = '42';
+var eggPrice = '41';
+var onionPriceNumber = parseInt(onionPrice);
+console.log(onionPriceNumber);
