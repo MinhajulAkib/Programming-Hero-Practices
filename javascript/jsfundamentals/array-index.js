56d5c2c67a1219d3bea152b4b270945d50ba4213
MinhajulAkib <00akibswe@gmail.com>
2021-08-01 20:00:11 +0600

    array index

---
+++ javascript/jsfundamentals/array-index.js
@@ -0,0 +1,9 @@
+var bookPages = ['akib', 'anik', 'saif','nobin', 'nayeem'];
+var namesIndex =bookPages.indexOf('nobin');
+console.log(namesIndex);
+
+
+var numbers = [11,13,45,67,54,65,47,445];
+console.log(numbers)
+numbers [2] =54;
+console.log(numbers);
