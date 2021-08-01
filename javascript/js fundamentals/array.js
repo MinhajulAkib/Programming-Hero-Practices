13e95e97fbe995df510f8ef8fe6db49e8898aba2
MinhajulAkib <00akibswe@gmail.com>
2021-08-01 19:01:38 +0600

    array

---
+++ javascript/js fundamentals/array.js
@@ -0,0 +1,8 @@
+// var friendsAge =[11, 13, 15];
+
+// // strings
+// var actress = ['mahi', 'opu', 'sabana'];
+
+// var oddNumbers = [1, 3, 5, 7, 9];
+var vowels =['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
