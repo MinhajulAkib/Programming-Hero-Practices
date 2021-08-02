c9875e3fb8fd7a79a4c1924c98a57bbc8f61dd35
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 12:52:43 +0600

    array

--- javascript/jsfundamentals/array.js
+++ javascript/jsfundamentals/array.js
@@ -1,19 +1,19 @@
-// var friendsAge =[11, 13, 15];
+var friendsAge =[11, 13, 15];
 
-// // strings
-// var actress = ['mahi', 'opu', 'sabana'];
+// strings
+var actress = ['mahi', 'opu', 'sabana'];
 
-// var oddNumbers = [1, 3, 5, 7, 9];
+var oddNumbers = [1, 3, 5, 7, 9];
 
 
-// var vowels =['a', 'e', 'i', 'o', 'u'];
-// console.log(vowels);
-// var vowelsCount = vowels.length;
-// console.log(vowelsCount);
+var vowels =['a', 'e', 'i', 'o', 'u'];
+console.log(vowels);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
 
 // last side in new value added
 var arrName = ['rahim', 'karim', 'jabbar', 'halim'];
-// console.log(arrName);
-// arrName.push = ('jodu');
+onsole.log(arrName);
+arrName.push = ('jodu');
 arrName.pop();
 console.log(arrName);
