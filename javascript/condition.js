5418e81a5c8d2cb1256906005641a64ac29e3a0a
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 11:30:40 +0600

    condition

---
+++ javascript/condition.js
@@ -0,0 +1,12 @@
+// if (3<5) {
+//     console.log ('yeah!, i will be excuated');
+// }
+
+
+var myName = 'minhajul';
+if (myName === 'minhaj'){
+    console.log('ok,executed')
+}
+else{
+    console.log('not ,it not minhaul');
+}
--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,29 @@
-console.log("it's working");
+// // console.log("it's working");
+// // var a = 100;
+// // var b = 3
+// // var c = a/b;
+// // console.log(c);
+
+// // comparison operator
+// var a =10;
+// var b = 20;
+// // var c = (a==b);
+// var d = (a != b);
+// // console.log(c);
+// console.log(d);
+
+
+// logical operator
+// &&, || , !
+/*---
+&&-all contion true-true
+one condition false ,another contion tru-false
+all condition false- FALSE
+*/
+
+var a = 10;
+var b= 20;
+var c= 30;
+var d = (b>a) && (c>b);
+console.log(d);
+
