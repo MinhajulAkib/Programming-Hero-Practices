ef2fd18d57d67e05fe95e7374d391f01f2e08c46
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 11:47:23 +0600

    condition

--- javascript/condition.js
+++ javascript/condition.js
@@ -3,10 +3,26 @@
 // }
 
 
-var myName = 'minhajul';
-if (myName === 'minhaj'){
-    console.log('ok,executed')
+// var myName = 'minhajul';
+// if (myName === 'minhaj'){
+//     console.log('ok,executed')
+// }
+// else{
+//     console.log('not ,it not minhaul');
+// }
+
+
+// else---if
+var whatsDay = 'sunday';
+if (whatsDay == 'friday'){
+    console.log("today is closed day");
+}
+else if (whatsDay == 'monday'){
+    console.log("today is partially closed");
+}
+else if(whatsDay == 'saturday'){
+    console.log("today is gov't holiday");
 }
 else{
-    console.log('not ,it not minhaul');
+    console.log("office day")
 }
