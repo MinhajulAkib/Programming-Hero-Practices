6ec36ef24ba8da71e52bbabb3ea12cf57a681983
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 12:49:45 +0600

    array

--- javascript/jsfundamentals/array.js
+++ javascript/jsfundamentals/array.js
@@ -4,7 +4,16 @@
 // var actress = ['mahi', 'opu', 'sabana'];
 
 // var oddNumbers = [1, 3, 5, 7, 9];
-var vowels =['a', 'e', 'i', 'o', 'u'];
-console.log(vowels);
-var vowelsCount = vowels.length;
-console.log(vowelsCount);
+
+
+// var vowels =['a', 'e', 'i', 'o', 'u'];
+// console.log(vowels);
+// var vowelsCount = vowels.length;
+// console.log(vowelsCount);
+
+// last side in new value added
+var arrName = ['rahim', 'karim', 'jabbar', 'halim'];
+// console.log(arrName);
+// arrName.push = ('jodu');
+arrName.pop();
+console.log(arrName);
