e6cd416456fa4fe1a00d735975a8c5811048b52e
MinhajulAkib <00akibswe@gmail.com>
2021-08-02 18:35:54 +0600

    loop

--- javascript/loop.js
+++ javascript/loop.js
@@ -1,11 +1,23 @@
-// // for loop
-// for (i=0; i<10; i++){
-//     console.log('go ' + i + ' step');
-// }
+// for loop
+for (i=0; i<10; i++){
+    console.log('go ' + i + ' step');
+}
 
-// while loop
+while loop
 var roastGiven = 0;
 while (roastGiven < 7){
     console.log('roast den please');
     roastGiven++;
 }
+
+var i = 0;
+while( i < 50){
+    if(i % 2 != 0){
+        console.log(i);
+    }
+    i++;
+}
+
+
+
+
