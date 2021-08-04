167a7196342353133b10e9ba31f1c25826bc1454
MinhajulAkib <00akibswe@gmail.com>
2021-08-04 11:11:04 +0600

    switch

---
+++ javascript/switch.js
@@ -0,0 +1,17 @@
+var weekDay = 'sunDay';
+switch (weekDay){
+    case 'saturDay' :
+        console.log('saturday! today is closed');
+        break;
+    case 'sunDay' :
+        console.log('saturday! mara day');
+        break;
+    case 'monDay' :
+        console.log('saturday! today is closed');
+        break;
+    case 'tuesDay' :
+        console.log('saturday! today is closed');
+        break;
+    default:
+    console.log('not a day');    
+}
