d45f86630513f5243a479408493162655125c4a1
MinhajulAkib <00akibswe@gmail.com>
2021-08-18 09:01:55 +0600

    event handler using addeventlistener

--- DOM Manipulation/Event-Handler/script.js
+++ DOM Manipulation/Event-Handler/script.js
@@ -6,11 +6,19 @@ function makeRed(){
 const bluebutton = document.getElementById('make-blue-button');
    bluebutton.onclick = makeblue;
    function makeblue(){
-       document.body.style.backgroundColor = 'blue';
+    document.body.style.backgroundColor = 'blue';
    }
 
-   //event handler by using anonymus function
-   const greenbutton = document.getElementById('make-green-button');
+//event handler by using anonymus function
+const greenbutton = document.getElementById('make-green-button');
    greenbutton.onclick=function(){
-       document.body.style.backgroundColor = 'green';
+    document.body.style.backgroundColor = 'green';
    }
+
+
+//add eventlistener
+const makebutton = document.getElementById('make-goldenrod-button');
+   makebutton.addEventListener('click', makegoldenrodbutton);
+   function makegoldenrodbutton(){
+    document.body.style.backgroundColor ='goldenrod';
+   }   
