f5f063c3112a8170dee41bf164f191afb58a7df8
MinhajulAkib <00akibswe@gmail.com>
2021-09-04 20:06:16 +0600

    setTimeout simple Asynchronous JS using

---
+++ browser and debug/timeout.js
@@ -0,0 +1,16 @@
+function doSomething() {
+    console.log('I am coding JavaScript');
+}
+console.log('First: ami sobar age');
+console.log('Second: I am the second person');
+// setTimeout(doSomething, 6000);
+//anonymus function
+setTimeout(function () {
+    console.log('I am using VS Code');
+}, 5000);
+//arrow function
+setTimeout(() => {
+    console.log('Exploring MDN articles');
+}, 4000);
+console.log('Third: ami 3 number baccha');
+console.log('Fourth: ami ek hali purno korsi');
