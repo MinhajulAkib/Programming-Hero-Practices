070e21b27f19c8673a2a20ac8d21f5cf7ccd84ed
MinhajulAkib <00akibswe@gmail.com>
2021-09-04 21:38:49 +0600

    Browser alert, confirm, prompt

--- browser-api/alert.js
+++ browser-api/alert.js
@@ -1,1 +1,27 @@
 console.log('I am here');
+
+
+// alert('ma is coming');
+
+const maComing = () => {
+    alert('ma is coming,open the book')
+}
+
+
+const askPicnic = () => {
+    const response = confirm('Are you going to picnic?');
+    console.log(response);
+    if (response === true) {
+        alert('amake fee ta bKash kore de');
+    }
+    else {
+        console.log('DGM !!! Door e giya mor!!!');
+    }
+}
+
+const askName = () => {
+    const name = prompt('What is your name?');
+    if (name) {
+        console.log(name)
+    }
+}
