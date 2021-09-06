61516307c46af95b0ede768efc68b8218d88bc96
MinhajulAkib <00akibswe@gmail.com>
2021-09-06 20:01:04 +0600

    object

---
+++ js/sec-2/max-min.js
@@ -0,0 +1,5 @@
+//let number = 1.789465;
+console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+console.log(Number.MIN_VALUE); // 5e-324
+console.log(Number.MAX_VALUE + Number.MAX_VALUE); // Infinity
+console.log(-Number.MAX_VALUE - Number.MAX_VALUE); // -Infinity
---
+++ js/sec-2/object.js
@@ -0,0 +1,14 @@
+let contact = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    phone: '(408)-555-9999',
+    address: {
+        building: '4000',
+        street: 'North 1st street',
+        city: 'San Jose',
+        state: 'CA',
+        country: 'USA'
+    }
+}
+console.log(contact.address.country);
