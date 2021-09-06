16e0ffc829fca215aa24f36d9b4b13466d541efd
MinhajulAkib <00akibswe@gmail.com>
2021-09-06 09:22:01 +0600

    array.js

---
+++ js/sec-1/array.js
@@ -0,0 +1,13 @@
+//using for
+
+let items = [1,2,3,4,5];
+for (let i =0; i <items.length; i++){
+    //console.log(items[i])
+}
+//console.log(items.length);
+
+//using for..of
+let data = [2,4,3,4,6,7,8,9,2];
+for(const dat of data){
+    console.log(dat);
+}
