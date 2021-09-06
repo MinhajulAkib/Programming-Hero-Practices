2f513d5008651c906fd3c06c8cd9abeeede405dc
MinhajulAkib <00akibswe@gmail.com>
2021-09-06 21:15:29 +0600

    umary operator

---
+++ js/sec-3/unary.js
@@ -0,0 +1,15 @@
+// let product = {
+//     valueOf: function () {
+//         return 60;
+//     }
+// };
+// console.log(+product); // 60
+
+// let i = 10, j = 20;
+// let m = --i + j;
+// console.log(m); // 30
+// console.log(i); // 9
+   
+
+let f1= 'BaNaNA'
+console.log(f1.toLowerCase());
