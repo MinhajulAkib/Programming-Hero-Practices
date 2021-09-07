0f1cac0af6a9a3abe50132984fb6e612ec9f76a9
MinhajulAkib <00akibswe@gmail.com>
2021-09-07 20:04:40 +0600

    html file

--- browser-api/browser-38.5/solving.js
+++ browser-api/browser-38.5/solving.js
@@ -1,3 +1,26 @@
 //১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। 
-console.log("start");
-setTimeout(() => console.log("hello"), 3500)
+// console.log("start");
+// setTimeout(() => console.log("hello"), 3500);
+
+
+//২. prompt দিয়ে ইউজারের কাছ থেকে একটা সংখ্যা নাও। সেটার সাথে ২০০যোগ করো। যোগ করতে গেলে parse করা লাগলে সেটা করো। তারপর সেই রেজাল্টকে alert দিয়ে আউটপুট দেখাও
+
+const data = prompt('Write a number!')
+alert(parseInt(data) + 200);
+
+// TODO: Task 3
+
+const confirmation = confirm('Do you want to see your website location')
+if (confirmation) {
+console.log(location=href);
+}
+const confirmation = confirm('Do you want to visit google')
+if (confirmation) {
+
+ location.href = 'https://www.google.com/';}
+
+// TODO: Task 4
+
+// ===================
+
+// TODO: Task 5
