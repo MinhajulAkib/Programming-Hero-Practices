69942a80e29b79c42b51fdffe07c5bb514617e15
MinhajulAkib <00akibswe@gmail.com>
2021-09-07 20:00:57 +0600

    set Time out

---
+++ browser-api/browser-38.5/solving.js
@@ -0,0 +1,3 @@
+//১. setTimeout এর ভিতরে একটা কিছু কনসোল লগ করবে যেটা সাড়ে তিন সেকেন্ড পরে আউটপুট দেখাবে। 
+console.log("start");
+setTimeout(() => console.log("hello"), 3500)
---
+++ js/sec-4/if-else.js
@@ -0,0 +1,23 @@
+// only register if the age is greater than 18
+//var age = 11;
+// var allowRegister = 11 > 18 ? true : false;
+// console.log(allowRegister);
+
+
+// var age = 19;
+// var canDrive = age > 16 ? 'yes' : 'no';
+// console.log(canDrive);
+
+// var authenticated = true;
+// var nextURL = authenticated ? (
+//     console.log('You will redirect to admin area'),
+//         '/admin'
+// ) : (
+//     console.log('Access denied'),
+//         '/403'
+// );
+// // redirect to nextURL here
+// console.log(nextURL); // '/admin'
+
+
+console.log(Math.floor(Math.random () * 100));
