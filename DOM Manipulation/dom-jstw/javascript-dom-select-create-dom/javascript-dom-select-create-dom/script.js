87bd760bfd6e07e2c8b3754df7c219d62aa33b46
MinhajulAkib <00akibswe@gmail.com>
2021-09-09 20:56:55 +0600

    dom

---
+++ DOM Manipulation/dom-jstw/javascript-dom-select-create-dom/javascript-dom-select-create-dom/script.js
@@ -0,0 +1,33 @@
+// var newparagraph = document.createElement('p');
+// //console.log(newparagraph);
+// var newdiv= document.createElement('div');
+// var chacchu = 'minhajul islam';
+// newparagraph.textContent = 'his name is :' + chacchu + '!';
+// console.log(newparagraph);
+
+
+// newdiv.innerHTML = '<h3>hllo bangladesh</h3>';
+// //new class add
+//  newdiv.classList.add('new-div');
+
+
+// var box1 = document.getElementById('box1');
+// box1.insertAdjacentElement('afterbegin', newdiv);
+
+
+
+
+
+// var newdiv = document.createElement('h3');
+// var chacchu = 'zunayed ahmed';
+// newdiv.textContent ='hsi name is' + chacchu + '!';
+// newdiv.innerHTML = '<h3>bangladesh</h3>';
+// //new class add
+//  newdiv.classList.add('new-div');
+//  var box1 = document.getElementById('box1');
+// box1.insertAdjacentElement('afterbegin', newdiv);
+// box2.insertAdjacentElement('afterend', newdiv);
+// box1.appendChild(box2);
+
+
+
