548199f026de3b0578982034e7a5b75e85427754
MinhajulAkib <00akibswe@gmail.com>
2021-09-13 09:08:56 +0600

    commit

--- DOM Manipulation/javascript-dom-event-handling/javascript-dom-event-handling/script.js
+++ DOM Manipulation/javascript-dom-event-handling/javascript-dom-event-handling/script.js
@@ -1,1 +1,25 @@
 // Start Writing Code Here
+//alert('hello world');
+
+function showAlert(){
+    alert('hello bangladesh');
+}
+
+
+const clickMeBtn = document.getElementById('btn-click-me');
+//console.log(clickMeBtn)
+clickMeBtn.onclick = function (){
+    console.log('click me please')
+}
+
+const buim = document.getElementById('box1');
+box1.addEventListener('click', function(){
+    console.log('clicked on box1');
+})
+
+clickMeBtn.onclick = function(event){
+    event.stopPropagation();
+    console.log('button');
+}
+
+const inputs = document.querySelectorAll('input');
