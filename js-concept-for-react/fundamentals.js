54df6537a68872c0709fa8999b5f364dbddfb802
MinhajulAkib <00akibswe@gmail.com>
2021-09-14 09:00:44 +0600

    array

--- js-concept-for-react/fundamentals.js
+++ js-concept-for-react/fundamentals.js
@@ -1,16 +1,25 @@
 //array
-let student = ['name', 'age', 'section', 'class'];
-// console.log(student);
+// let student = ['name', 'age', 'section', 'class'];
+//console.log(student);
 // console.log(student[1]);
 // console.log(student.length);
 
-let first = student[0]
-let last = student[student.length -1];
+// let first = student[0]
+// let last = student[student.length -1];
 // console.log(first);
 // console.log(last);
 
 
 //loop  over an array
-student.forEach(function(item, index, array) {
-    console.log(item, index,array)
-  })
+// student.forEach(function(item, index, array) {
+//     //console.log(item, index,array)
+// })
+
+
+//add an item to the end of an array
+let student = ['name', 'age', 'section', 'class'];
+let newadd = student.push('Orange');
+console.log(newadd);
+
+let removedItem = student.splice(1);
+console.log(removedItem);
