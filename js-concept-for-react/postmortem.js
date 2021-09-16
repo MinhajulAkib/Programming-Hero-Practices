cec81e29be855bf11a86b68c34a797a4c4e254f9
MinhajulAkib <00akibswe@gmail.com>
2021-09-16 20:31:16 +0600

    object

---
+++ js-concept-for-react/postmortem.js
@@ -0,0 +1,15 @@
+//array
+// const result = [1,2,3,4,5,6,7,8,9,10,11];
+//  const newarray = [...result, "hero alom"];
+//  console.log(newarray);
+
+
+//  var a =['dog', 'cat', 'hen'];
+//  a[100] = 'fox';
+//  console.log(a.length);
+
+
+
+const x = 6%2;
+const y = x ? 'One': 'Two';
+console.log(y);
